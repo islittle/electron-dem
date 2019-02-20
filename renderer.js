2d568cf1c5110300dd386327f499043c232a94d7
王峰 <wangfeng01@yit.com>
2019-02-20 11:35:09 +0800

    桌面应用

---
+++ renderer.js
@@ -0,0 +1,8 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+// index.html文件需要此文件
+//在该窗口的渲染器进程中执行。
+//此过程中提供了所有Node.js API。 
+//解释：用来放业务相关JS的，和网页JS的区别在于，这里的JS不仅可以访问DOM，还能使用Node.js所有的AP
+// alert(JSON.stringify(app))
